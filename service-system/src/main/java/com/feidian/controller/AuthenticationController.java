732658d1a6680c39abbdc47cb2c12d0142fcf0a2
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-07-12 14:40:31 +0800

    refactor(service-system):LoginController完成了它的使命，永远离开了我们

--- service-system/src/main/java/com/feidian/controller/AuthenticationController.java
+++ service-system/src/main/java/com/feidian/controller/AuthenticationController.java
@@ -1,10 +1,13 @@
 package com.feidian.controller;
 
+import com.feidian.dto.LoginDTO;
 import com.feidian.dto.SignupDTO;
+import com.feidian.po.SysUser;
 import com.feidian.responseResult.ResponseResult;
 import com.feidian.service.AuthenticationService;
 import com.feidian.service.UtilService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +21,36 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
+
+    //邮箱注册
+    @PostMapping("/emailSignup")
+    public ResponseResult emailSignup(@RequestBody SignupDTO signupDTO) throws Exception {
+        return authenticationService.emailSignup(signupDTO);
+    }
+
     //发送验证码
     @PostMapping("/sendVerifyCode")
     public ResponseResult sendVerifyCode(@RequestBody SignupDTO signupDTO) {
         return utilService.sendVerifyCode(signupDTO.getEmailAddress(), signupDTO.getUsername());
     }
+
+    @PostMapping("/user/login")
+    public ResponseResult login(@RequestBody LoginDTO user){
+        return authenticationService.login(user);
+    }
+
+    @GetMapping("/user/logout")
+    public ResponseResult logout(){
+        return authenticationService.logout();
+    }
+
+    //注册登录
+    //快速注册
+    @PostMapping("/fastSignup")
+    public ResponseResult fastSignup(@RequestBody SignupDTO signupDTO){
+        return authenticationService.fastSignup(signupDTO);
+    }
+
+
+
 }
