aae0079500066235299c4f73af616079c8578255
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-07-12 10:50:39 +0800

    fix(service-system):修复了userMapper

--- service-system/src/main/java/com/feidian/mapper/UserMapper.java
+++ service-system/src/main/java/com/feidian/mapper/UserMapper.java
@@ -1,11 +1,22 @@
 package com.feidian.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.feidian.bo.UserBO;
 import com.feidian.po.SysUser;
+import com.feidian.po.UserPO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface UserMapper extends BaseMapper<SysUser> {
+
+    void insertUser(UserBO userBO);
+
+    UserPO findByName(String username);
+
+    void updateUserInfo(UserBO userBO);
+
+    UserPO findById(long id);
+
 }
